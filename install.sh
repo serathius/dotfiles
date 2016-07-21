efbfa12261dbb9e6c86b176a5b9260841f535847
Marek Siarkowicz <mareksiarkowicz@gmail.com>
2016-07-21 22:38:38 +0200

    Migrate to i3blocks.

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -e
+sudo apt-get install i3blocks
 pushd $(dirname $0) > /dev/null
   SCRIPTPATH=$(pwd)
 popd > /dev/null
 pushd ~
-  ln -sf "$SCRIPTPATH/i3status.conf" .i3status.conf
+  ln -sf "$SCRIPTPATH/i3blocks.conf" .i3blocks.conf
 popd
 mkdir -p ~/.i3
 pushd ~/.i3
   ln -sf "$SCRIPTPATH/i3.conf" config
 popd 
+i3-msg restart
+
