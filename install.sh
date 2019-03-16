ea234c211125947aa80fd8d30e781db911f9df9b
Marek Siarkowicz <serathius@users.noreply.github.com>
2019-03-16 16:25:52 +0100

    Add bashrc and it's sources and improve installation

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
 #!/bin/bash -e
+
 pushd $(dirname $0) > /dev/null
   SCRIPTPATH=$(pwd)
 popd > /dev/null
-pushd ~
-  ln -sf "$SCRIPTPATH/i3blocks.conf" .i3blocks.conf
-popd
+
+function install_file {
+  SRC=$1
+  DST=$2
+  [[ ! -s $DST ]] && mv $DST $DST.old
+  ln -sf $SRC $DST
+}
+install_file "$SCRIPTPATH/i3blocks.conf" ~/.i3blocks.conf
+install_file "$SCRIPTPATH/.bashrc" ~/.bashrc
+install_file "$SCRIPTPATH/.prompt" ~/.prompt
+install_file "$SCRIPTPATH/.wallpaper.jpg" ~/.wallpaper.jpg
+install_file "$SCRIPTPATH/.bash_aliases" ~/.bash_aliases
+
 mkdir -p ~/.config/i3/
-pushd ~/.config/i3/
-  ln -sf "$SCRIPTPATH/.config/i3/config" config
-popd 
+install_file "$SCRIPTPATH/.config/i3/config" ~/.config/i3/config
 i3-msg restart
 
