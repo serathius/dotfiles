3e2b16d83b7d1d4e0487c7b490fe4a5e7847d2a8
Marek Siarkowicz <serathius@users.noreply.github.com>
2019-03-17 01:13:59 +0100

    Improve i3lock with using https://github.com/Ema0/i3lock-fancy

---
+++ .config/i3/scripts/lock.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+IMAGE=/tmp/i3lock.png
+TEXT="locked"
+RESOURCES=/home/serathius/.config/i3/resources
+RES=$(xrandr --current | grep '*' | uniq | awk '{print $1}')
+BOXSIZE=$(expr $(echo $RES | cut -d 'x' -f1) / 2)
+
+if [[ $1 != "" ]]; then
+    TEXT=$1
+fi
+
+ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $RESOURCES/lock.png -filter_complex "boxblur=5,drawtext=fontfile=$RESOURCES/Digital_tech.otf:text=$TEXT:fontcolor='#ffffffe0':fontsize=40:x=(w-tw)/2:y=(h/PHI)+th:shadowcolor='#1d1f21d0':shadowx=2:shadowy=2:box=1:boxcolor=#1a1b22@0.4:boxborderw=$BOXSIZE,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $IMAGE
+
+i3lock -i $IMAGE
+
+rm $IMAGE
--- install.sh
+++ install.sh
@@ -23,12 +23,15 @@ install_file "$SCRIPTPATH/.config/i3/config" ~/.config/i3/config
 install_file "$SCRIPTPATH/.config/i3/i3blocks.conf" ~/.config/i3/i3blocks.conf
 mkdir -p ~/.config/i3/scripts
 install_file "$SCRIPTPATH/.config/i3/scripts/wallpaper.sh" ~/.config/i3/scripts/wallpaper.sh
+install_file "$SCRIPTPATH/.config/i3/scripts/lock.sh" ~/.config/i3/scripts/lock.sh
+mkdir -p ~/.config/i3/resources
+cp $SCRIPTPATH/.config/i3/resources/* ~/.config/i3/resources
 
 mkdir -p ~/.config/terminator/
 install_file "$SCRIPTPATH/.config/terminator/config" ~/.config/terminator/config
 
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install terminator i3 i3blocks redshift-gtk keepass2 pass undistract-me steam python3-dev python3-pip rofi feh compton
+sudo apt-get -y install terminator i3 i3blocks redshift-gtk keepass2 pass undistract-me steam python3-dev python3-pip rofi feh compton ffmpeg
 sudo pip3 install virtualenvwrapper
 sudo dpkg-reconfigure keyboard-configuration
